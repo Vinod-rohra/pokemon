a2d33856bfd4b1d2cca6e7484814193fa3cd1552
vinod-rohra <VRohra@VRohra>
2020-12-11 20:47:45 -0500

    test

---
+++ src/Partay.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Partay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Pokemon 1:");
+		String pokemon1 = scan.nextLine();
+		
+		System.out.println("Enter Pokemon 2:");
+		String pokemon2 = scan.nextLine();
+	
+		
+		System.out.println("poke1:" + pokemon1);
+		
+		System.out.println("poke2:" + pokemon2);
+	}
+
+}
